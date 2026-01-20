9c618488a27177598bc386af8f7f580a673a6db6
foryutvofficial-ctrl <foryutvofficial@gmail.com>
2026-01-19 19:55:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -9,17 +9,20 @@ const seriesData = {
   series1: {
     name: "You Are Desire",
     trailer: "youaredesire.mp4", // optional local video
-    episodes: ["Episode 1","Episode 2","Episode 3","Episode 4"]
+    episodes: ["Episode 1","Episode 2","Episode 3","Episode 4","Episode 5","Episode 6","Episode 7","Episode 8","Episode 9",
+               "Episode 10","Episode 11","Episode 12","Episode 13","Episode 14","Episode 15","Episode 16","Episode 17",
+               "Episode 18","Episode 19","Episode 20","Episode 21","Episode 22","Episode 23","Episode 24","Episode 25","Episode 26"
+    ,"Episode 27","Episode 28","Episode 29","Episode 30"]
   },
   series2: {
     name: "Reply 1988",
     trailer: "reply1988.mp4",
-    episodes: ["Episode 1","Episode 2","Episode 3"]
+  
   },
   series3: {
     name: "The Secret of Us",
     trailer: "thesecretofus.mp4",
-    episodes: ["Episode 1","Episode 2"]
+   
   }
 };
 
